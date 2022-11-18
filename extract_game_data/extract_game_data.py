907e23b18742e6a3a4d505565b22c4455530f84e
evancoleslewis <evancoles14@gmail.com>
2022-11-18 15:26:30 -0800

    Reformat extract_game_data.py

--- extract_game_data/extract_game_data.py
+++ extract_game_data/extract_game_data.py
@@ -1,6 +1,7 @@
-from bs4 import BeautifulSoup
-import pandas as pd
 from datetime import datetime 
+from bs4 import BeautifulSoup
+
+import pandas as pd
 import numpy as np
 import requests 
 import re
