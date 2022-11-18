a17198ae900255f83fa51515f0d12a73e305adfd
evancoleslewis <evancoles14@gmail.com>
2022-11-18 15:41:28 -0800

    Add very first version of main.py WIP

---
+++ main.py
@@ -0,0 +1,11 @@
+from extract_game_data import get_all_games_on_date
+
+def main(game_date : str) ->:
+
+    game_dict = get_all_games_on_date(game_date)
+
+    # To Do: create dataframe based on each dict
+
+    # To Do: write data to csv (eventually to database)
+
+    return
