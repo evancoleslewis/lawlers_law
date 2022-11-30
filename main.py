05ba450f22c78c7ed9a8e4c30ffa46b5d0b0498b
evancoleslewis <evancoles14@gmail.com>
2022-11-29 21:44:00 -0800

    Set game_date to yesterday

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
 from extract_game_data.extract_game_data import get_all_games_on_date
-from format_game_data.build_game_df import build_game_df
+from format_game_data.build_game_df import build_all_games_df
 import pandas as pd
+from datetime import datetime, timedelta
 
 def main():
     
-    # TODO: Set to yesterday
-    game_date = '2022-11-22'
+    game_date = (datetime.today() - timedelta(days=1))  # get yesterday's date
+    all_games_dict = get_all_games_on_date(game_date)  # get all games from yesterday
+    all_games_df = build_all_games_df(all_games_dict)  # format data into dataframe
 
-    game_date_dict = get_all_games_on_date(game_date)
-    
-    # TODO: clean up dataframe concatenation
-    game_dict = game_date_dict[game_date]
-    df_dict = dict()
-    for home_team in game_dict.keys():
-        game_df = build_game_df(game_dict[home_team])
-        df_dict[home_team] = game_df.copy()
-    
-    all_games_df = pd.concat(df_dict).reset_index(drop=True)
-    
     # TODO: write data to csv (eventually to database)
-    all_games_df.to_csv(f'temp_csvs/lawler_{game_date}.csv', index=False)
+    all_games_df.to_csv(f'csv/lawler_{game_date.strftime("%Y-%m-%d")}.csv', index=False)  # write data to csv
     
     return
 
