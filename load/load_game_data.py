a09fe79250732661a0b3f6dbe524919110804e83
evancoleslewis <evancoles14@gmail.com>
2023-01-04 17:41:32 -0800

    Add capability for concatenating all dfs

--- load/load_game_data.py
+++ load/load_game_data.py
@@ -2,19 +2,43 @@ import os
 import pandas as pd
 from datetime import datetime
 
-def concat_all_csv(file_path):
+def concat_all_csv(file_path : str
+                    ,all_csvs : list) -> list:
+
+    """
+    Reads all csv file names into dataframes. 
+    
+    Returns all dataframes concatenated into one dataframe.
+    """
+
+    df_dict = dict()
+
+    for csv in all_csvs:
+        try:
+            df = pd.read_csv(file_path+csv)
+            df_dict[csv] = df.copy()
+        except:
+            print(f"Unable to read {csv}")
+        
+    all_games_df = pd.concat(df_dict).reset_index(drop=True)  # concat all into 1 df
+    all_games_df.drop_duplicates(inplace=True)  # remove any duplicate rows
+
+    return all_games_df
+
+
+def write_all_csv(file_path):
     """
     Reads all csvs in the given file_path and concatenates them.
     Writes concatenated df to csv and creates a backup with timestamp.
     """
-
-    all_csvs = [file for file in os.list_dir(file_path) if '.csv' in file]  # gets list of all csv names
-    all_dfs = [pd.read_csv(file_path+file) for file in all_csvs]  # get list of all dataframes
     
-    df_all_games = pd.concat(all_dfs)  # concat all dataframes into one
-    df_all_games.drop_duplicates(inplace=True)  # remove any duplicate rows
+    all_csvs = [file for file in os.listdir(file_path) if '.csv' in file]  # gets list of all csv names
+    all_games_df = concat_all_csv(file_path, all_csvs)  # read and concat all csvs
 
-    df_all_games.to_csv(file_path+'lawler.csv', index=False)  # write all games to csv
-    df_all_games.to_csv(file_path+f'/backup/lawler_{str(datetime.now())}.csv', index=False)  # create backup based on current time
+    all_games_df.to_csv(file_path+'lawler.csv', index=False)  # write all games to csv
+    all_games_df.to_csv(file_path+f'backup/lawler_{str(datetime.now())}.csv', index=False)  # create backup based on current time
 
     return
+
+csv_file_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data/csv/'))  # get cwd, go one level up, and join data/csv to get full path
+write_all_csv(csv_file_path+'/')  # run function with csv_file_path
